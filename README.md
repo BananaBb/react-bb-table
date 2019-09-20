ca73f15c0291160ccc4a4839a014fe7ed2ba86cd
bananabb <samson8682@gmail.com>
2019-09-20 19:04:53 +0800

    ADD: restructure project

--- README.md
+++ README.md
@@ -14,13 +14,76 @@ npm install --save react-bb-table
 
 ```jsx
 import React, { Component } from 'react'
+import ReactBbTable from 'react-bb-table'
 
-import MyComponent from 'react-bb-table'
+let options  = {
+    dimensions: {
+        width: "100%"
+    },
+    data: {
+        columns: [ 
+            {
+                id: "id",
+                label: "Action",
+                colClass: "actionCell",
+                unique: {
+                    view: {path: "./view/", item: "Preview"},
+                    edit: {path: "./edit/", item: "Modify"},
+                    del: {path: "./del/", item: "Delete"}
+                }
+            },
+            {
+                id: "file_name",
+                label: "File Name",
+                colClass: "fileCell"
+            },
+            {
+                id: "doc_type",
+                label: "Document Type",
+                colClass: "docCell"
+            },
+            {
+                id: "modify_date",
+                label: "Last Modify Date",
+                colClass: "dateCell"
+            },
+            {
+                id: "confident_level",
+                label: "Confident Level",
+                colClass: "lvCell"
+            },
+            {
+                id: "page_num",
+                label: "Scanned Page",
+                colClass: "pageCell"
+            },
+            
+        ],
+        rows: [
+            {
+                id: 1,
+                file_name: "File 1",
+                doc_type: "document",
+                modify_date: "28/08/2019, 00:00",
+                confident_level: "98% confidence",
+                page_num: "5 Pages",
+            },
+            {
+                id: 2,
+                file_name: "File 2",
+                doc_type: "execl",
+                modify_date: "28/08/2019, 00:01",
+                confident_level: "99% confidence",
+                page_num: "148 Pages",
+            },
+        ],
+    }
+}
 
-class Example extends Component {
+export default class App extends Component {
   render () {
     return (
-      <MyComponent />
+    	<ReactBbTable options={options} />
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,86 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-
 import styles from './styles.css'
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
+
+export default class ReactBbTable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      options: this.props.options,
+      recordID: 0
+    };
+    this.settings = [];
+    this.rowLinks = "";
+  }
+
+  replaceRowID(id) {
+    return (<div dangerouslySetInnerHTML={{__html: this.rowLinks.replace(/%id%/g, id)}} />)
+  }
+
+  tableRowLink(unique) {
+    for (let key in unique) {
+      this.rowLinks+= `<a href="%path%%id%" class="bb-table-row-cell-btn" key="link-%key%-%id%">%item%</a>`
+        .replace("%path%", unique[key].path)
+        .replace("%key%", key)
+        .replace("%item%", unique[key].item)
+    }
+    return ""
+  }
+
+  tableHeader(cols) {
+    if (cols.length <= 0) {
+      return (<div />)
+    }
+    return (
+      <div className="bb-table-header">
+        {cols.map((col) => {
+          if (col.unique) {
+            this.tableRowLink(col.unique)
+          }
+          let styleclass = (col.colClass) ? "bb-table-row-cell " + col.colClass : "bb-table-row-cell"
+          this.settings.push({
+            'unique': (col.unique) ? true : false,
+            'key': col.id,
+            'style': styleclass
+          })
+          return (<div className={styleclass} key={"header-" + col.id}>{col.label}</div>)
+        })}
+      </div>
+    )
+  }
+
+  tableRow(rows) {
+    if (rows.length <= 0 || this.settings.length <= 0) {
+      return (<div className="bb-table-no-record">No Records</div>)
+    }
+    return (
+      <div className="bb-table-row-group">
+        {rows.map((row, idx) => {
+          return (
+            <div className="bb-table-row" key={"row-" + idx}>
+              {this.settings.map((setting) => {
+                return (
+                  <div className={setting.style} key={idx+"-"+setting.key}>
+                    {(setting.unique) ? this.replaceRowID(row[setting.key]) : <div>{row[setting.key]}</div>}
+                  </div>
+                )
+              })}
+            </div>
+          )
+        })}
+      </div>
+    )
   }
 
   render() {
-    const {
-      text
-    } = this.props
-
+    if (!this.state.options.data) {
+      return (<div />)
+    }
     return (
-      <div className={styles.test}>
-        Example Component: {text}
+      <div className="bb-table" style={(this.state.options.dimensions.width) ? {width: this.state.options.dimensions.width} : {}}>
+        {this.tableHeader(this.state.options.data.columns)}
+        {this.tableRow(this.state.options.data.rows)}
       </div>
     )
   }
